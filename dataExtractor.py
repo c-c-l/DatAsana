93da7a7fee01aa78815d2d56767c87011d24c541
Cecilia Macioszek <cecilia.mcszk@gmail.com>
2018-04-26 17:02:10 +0200

    [WIP] add collecting data script

---
+++ dataExtractor.py
@@ -0,0 +1,28 @@
+# Python 3
+# Creates a csv file parsing data need from the web
+# Author : Cecilia Macioszek (GitHub : c-c-l, Instagram : this_is_ccl)
+
+import csv
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+# Constant variables
+TYPEURL = 'https://www.yogajournal.com/poses/types'
+BENEFITURL = 'https://www.yogajournal.com/poses/yoga-by-benefit'
+
+
+# url : url to be parsed
+def htmlparser(url):
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    parent = soup.find('section', {'class': 'm-card-group'})
+    print(parent)
+
+
+htmlparser(TYPEURL)
