a0b347a1aca7b1d29b7e76f5d0e908036f39983c
Cecilia Macioszek <cecilia.mcszk@gmail.com>
2019-02-06 16:29:08 +0100

    delete duplicates, fix some code, add poses names
    - URGENT NEED TO DRY BC EVERYTHING IS SUPER MESSY (note to myself)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,3 @@
-
-// console.log(sanskritList);
-// console.log(types);
-
-// console.log(benefitsListUnclean.length);
-// Clean benefits lists (del dup)
-// for(var bIndex = 0; bIndex < benefitsListUnclean.length; bIndex++) {
-//     console.log(bIndex);
-// }
-// DataSet
-//// Get benefits
-// for(var dataIndex = 0; dataIndex < dataset.length; dataIndex++) {
-//     console.log(dataIndex);
-// }
-
 ////////////////////////////////////////////////////////////
 //////////////////////// Set-Up ////////////////////////////
 ////////////////////////////////////////////////////////////
@@ -35,7 +20,6 @@ var svg = d3.select("#chart").append("svg")
           .append("g")
           .classed('wrapper', true)
           .attr("transform", "translate(" + (width/2 + margin.left) + "," + (height/2 + margin.top) + ")");
-// .datum(chord(matrix));
 var svg = d3.select(' .wrapper');
 
 svg.append('defs');
@@ -55,10 +39,9 @@ svgBase.select('feMerge')
 
 var outerGroup = svg.append('g').classed('outer', true);
 var innerGroup = svg.append('g').classed('inner', true);
-// innerGroup.attr('transform', 'translate(100,100)');
 
 d3.csv("https://raw.githubusercontent.com/c-c-l/DatAsana/master/DataCollect/yoga_data.csv", function(data) {
-    console.log(data);
+    // console.log(data);
     var benefitsList= [];
     data.forEach(function(d) {
         benefitsList.push(d.Benefits);
@@ -78,8 +61,9 @@ d3.csv("https://raw.githubusercontent.com/c-c-l/DatAsana/master/DataCollect/yoga
                 split[splitIdx] = split[splitIdx].replace('\'', '');
                 split[splitIdx] = split[splitIdx].replace('\'', '');
 
-                // Remove space
-                split[splitIdx] = split[splitIdx].replace(' ', '');
+                // Trim data
+                split[splitIdx] = split[splitIdx].trim();
+
                 // Add all elements to array
                 bList.push(split[splitIdx]);
             }
@@ -87,12 +71,42 @@ d3.csv("https://raw.githubusercontent.com/c-c-l/DatAsana/master/DataCollect/yoga
     }
     // Clean new list to del dup
     var benefits = Array.from(new Set(bList));
-    console.log(benefits);
+
+
+    var posesList= [];
+    data.forEach(function(d) {
+        posesList.push(d.Sanskrit);
+    });
+    // Clean list
+    var sList = [];
+    for(var sIndex = 0; sIndex < posesList.length; sIndex++) {
+        if(posesList[sIndex] != '[]') { // not empty
+            var split = posesList[sIndex].split(',');
+            for(var splitIdx = 0; splitIdx < split.length; splitIdx++) {
+
+                // Remove square bracket
+                split[splitIdx] = split[splitIdx].replace('[', '');
+                split[splitIdx] = split[splitIdx].replace(']', '');
+
+                // Remove single quotes
+                split[splitIdx] = split[splitIdx].replace('\'', '');
+                split[splitIdx] = split[splitIdx].replace('\'', '');
+
+                // Trim data
+                split[splitIdx] = split[splitIdx].trim();
+
+                // Add all elements to array
+                sList.push(split[splitIdx]);
+            }
+        }
+    }
+    // Clean new list to del dup
+    var poses = Array.from(new Set(sList));
 
     //Creates a function that makes SVG paths in the shape of arcs with the specified inner and outer radius
-	var arc = d3.arc()
-			    .innerRadius(width*0.9/2)
-			    .outerRadius(width*0.9/2 + 30);
+    var arc = d3.arc()
+                .innerRadius(width*0.9/2)
+			          .outerRadius(width*0.9/2 + 30);
 
     var arcIn = d3.arc()
                   .innerRadius((width*0.9/2) - 200)
@@ -115,16 +129,40 @@ d3.csv("https://raw.githubusercontent.com/c-c-l/DatAsana/master/DataCollect/yoga
           .attr("cy", function(d) { var centroid = arcIn.centroid(d); return centroid[1]; })
           .attr("class", "posesArc")
           .attr("id", function(d,i) { return "posesArc-"+i; });
+          var circles = svg.selectAll(' .inner circle');
+          var groupCircles = circles._groups[0];
+          for (var circleIdx = 0; circleIdx < groupCircles.length; circleIdx++) {
+              var id = getIndex(getId(groupCircles[circleIdx]));
+              var circle = d3.select(groupCircles[circleIdx]);
+              var sanskrit = data[circleIdx].Sanskrit.toLowerCase().split(' ').join('');
+              // console.log(sanskrit);
+              svg.select(' .inner').append("text")
+                 .attr("class", "posesText")
+                 .classed(sanskrit + '-text', true)
+                 .text(function(d) {
+                   return poses[circleIdx];
+                 })
+                 .style('fill', '#fff')
+                 .attr("x", function (d) {
+                     var x = circle.attr('cx');
+                     return x;
+                 })
+                 .attr("y", function (d) {
+                     var y = parseFloat(circle.attr('cy'));
+                     return y + 5 ; // 5 is offset
+                 });
+          };
 
     var tip = d3.select("body").append("div")
          .attr("class", "tooltip")
          .style("opacity", 0);
-    // Write benefits
+
+    // Write points & text
     outerGroup.selectAll(" .benefitsText")
               .data(pie(benefits))
               .enter()
               .append("circle")
-              .attr('r', "4")
+              .attr('r', "6")
               .attr("cx", function(d) { var centroid = arc.centroid(d); return centroid[0]; })
               .attr("cy", function(d) { var centroid = arc.centroid(d); return centroid[1]; })
               .attr("class", "benefitArc")
@@ -136,20 +174,48 @@ d3.csv("https://raw.githubusercontent.com/c-c-l/DatAsana/master/DataCollect/yoga
               for (var circleIdx = 0; circleIdx < groupCircles.length; circleIdx++) {
                   var id = getIndex(getId(groupCircles[circleIdx]));
                   var circle = d3.select(groupCircles[circleIdx]);
-                  svg.append("text")
+                  svg.select(' .outer').append("text")
+                     .attr("class", "benefitText")
                      .text(function(d) {
-                         return benefits[circleIdx];
+                       return benefits[circleIdx];
                      })
                      .style('fill',function(d) { return colors[circleIdx];} )
                      .attr("x", function (d) {
                          var x = circle.attr('cx');
-                         return x - 25;
+                         return x;
                      })
                      .attr("y", function (d) {
-                         var y = circle.attr('cy');
-                         return y - 10;
+                         var y = parseFloat(circle.attr('cy'));
+                         return y + 5 ; // 5 is offset
                      });
               };
+
+      // Fix text position
+      // Build list of text elements width
+      var allText = document.getElementsByClassName('benefitText');
+      var lenTextList = [];
+      for (var textEl = 0; textEl < allText.length; textEl++) {
+        // Push the width of current element in the array
+        lenTextList.push(allText[textEl].getBBox().width);
+      }
+      var textAll = svg.selectAll("text.benefitText");
+      var groupText = textAll._groups[0];
+      for(var textIdx = 0; textIdx < groupText.length; textIdx++) {
+        var text = d3.select(groupText[textIdx]);
+        text.attr("x", function(d) {
+          var x = parseFloat(text.attr("x"));
+          if (Math.sign(text.attr("x")) === -1) {
+            return x - lenTextList[textIdx] - 15; // 15 is offset
+          }
+          else {
+            return x + 10; // 10 is offset
+          }
+        });
+      };
+
+
+
+
      //              tip.transition()
      //              .duration(200)
      //              .style("opacity", .9);
@@ -194,10 +260,13 @@ d3.csv("https://raw.githubusercontent.com/c-c-l/DatAsana/master/DataCollect/yoga
                 var x2 = d3.select(' #benefitArc-' + beneIdx).attr('cx');
                 var y1 = d3.select(' #posesArc-' + poseIdx).attr('cy');
                 var y2 = d3.select(' #benefitArc-' + beneIdx).attr('cy');
-                var className = benefits[beneIdx].toLowerCase();
-                className = className.split(' ').join('');
-                svg.append('line').classed(className + '-line', true)
+                var classNameB = benefits[beneIdx].toLowerCase();
+                classNameB = classNameB.split(' ').join('');
+                var classNameS = data[poseIdx].Sanskrit.toLowerCase();
+                classNameS = classNameS.split(' ').join('');
+                svg.append('line').classed(classNameB + '-line', true)
                    .classed('line', true)
+                   .classed(classNameS + '-line', true)
                    .attr('x1', x1)
                    .attr('x2', x2)
                    .attr('y1', y1)
@@ -208,20 +277,37 @@ d3.csv("https://raw.githubusercontent.com/c-c-l/DatAsana/master/DataCollect/yoga
     }
     svg.selectAll('circle')
               .on("mouseover", function(d, i) {
-                  console.log('mouseover' + i);
+                  // console.log('mouseover' + i);
                   var benefit = d.data;
                   benefit = benefit.toLowerCase();
                   benefit = benefit.split(' ').join('');
-                  console.log(benefit);
+                  // console.log(benefit);
                   svg.selectAll('line').style('stroke-opacity', 0);
                   displayLines(benefit);
+                  displaySanskrit(benefit);
               })
               .on('mouseout', function() {
                   svg.selectAll('line').style('stroke-opacity', 0.45);
+                  svg.selectAll(' .posesText').style('fill-opacity', '0');
               });
     // Display lines corresponding to benefit
     function displayLines(benefit) {
         svg.selectAll('line.' + benefit + '-line').style('stroke-opacity', 0.45);
+    }
+    // Display poses corresponding to lines
+    function displaySanskrit(benefit) {
+      var line = svg.selectAll('line.' + benefit + '-line')._groups[0];
+      for (var pIdx = 0; pIdx < posesList.length; pIdx++) {
+        var sanskrit = posesList[pIdx].toLowerCase().split(' ').join('');
+        for(var lineIdx = 0; lineIdx < line.length; lineIdx++) {
+          if(line[lineIdx].classList[2] === sanskrit +'-line'){
+            var color = line[lineIdx].style.stroke;
+            svg.select('text.'+ sanskrit + '-text').style('fill-opacity', 1);
+            svg.select('text.'+ sanskrit + '-text').style('fill', color);
+          }
+        }
+      }
+      svg.selectAll('text.' + sanskrit + '-text').style('fill-opacity', 1);
     }
     // Get index from id name
     function getIndex(id) {
